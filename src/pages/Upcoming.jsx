29373e3b47644100f5ab7a681b1675683ff9398e
Jenith1304 <jenithpanchal1304@gmail.com>
2024-10-28 23:20:33 +0530

    Upcoming Interview Design

---
+++ src/pages/Upcoming.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Upcoming = () => {
+    return (
+        <div>Upcoming
+            {console.log("Hello World")}
+        </div>
+    )
+}
+
+export default Upcoming
