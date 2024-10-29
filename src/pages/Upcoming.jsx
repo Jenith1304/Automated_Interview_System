9d467b2d181cd67d02b6fd016d4e6300fee030e9
Jenith1304 <jenithpanchal1304@gmail.com>
2024-10-29 23:46:49 +0530

    UpcomingInterviewComplete

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import InterviewCards from '../components/InterviewCards'
+import InformationInput from '../components/InformationInput'
+import styles from '../styles/Upcoming.module.css'
 
 const Upcoming = () => {
     return (
-        <div>Upcoming
-            {console.log("Hello World")}
+        <div className={styles.upcoming}>
+            <div className={styles.upcomingHeader}>
+                <h2> Upcoming Interviews </h2>
+                <div>
+                    <InformationInput type="password" placeholder="Search" style={{ width: "100%" }} />
+                </div>
+            </div>
+            <hr />
+            <InterviewCards />
+
         </div>
     )
 }
