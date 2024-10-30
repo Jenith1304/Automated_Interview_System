8c7ea4f961e5a15b941ed75bd6645cceb3e04dfd
Jenith1304 <jenithpanchal1304@gmail.com>
2024-10-31 00:55:20 +0530

    Cards Dynamic Design For Upcoming and Current

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -4,16 +4,32 @@ import InformationInput from '../components/InformationInput'
 import styles from '../styles/Upcoming.module.css'
 
 const Upcoming = () => {
+    const details = [
+        {
+            name: "Ratan Tata",
+            imgLink: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Ratan_Tata_photo.jpg",
+            jobRole: "JuniorScientist",
+            time: "13:00 a.m.",
+            date: "23rd June,2024"
+        },
+        {
+            name: "Ratan Tata 2",
+            imgLink: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Ratan_Tata_photo.jpg",
+            jobRole: "JuniorScientist",
+            time: "13:00 a.m.",
+            date: "23rd June,2024"
+        },
+    ]
     return (
         <div className={styles.upcoming}>
             <div className={styles.upcomingHeader}>
                 <h2> Upcoming Interviews </h2>
                 <div>
-                    <InformationInput type="password" placeholder="Search" style={{ width: "100%" }} />
+                    <InformationInput type="text" placeholder="Search" style={{ width: "100%" }} />
                 </div>
             </div>
             <hr />
-            <InterviewCards />
+            <InterviewCards type={"upcoming"} details={details} />
 
         </div>
     )
