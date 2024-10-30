2f72bb1d2fd9f9a4036736705a5b82870095c3db
Jenith1304 <jenithpanchal1304@gmail.com>
2024-10-31 00:52:06 +0530

    Cards Dynamic Design For Upcoming and Current

--- src/components/InterviewCards.jsx
+++ src/components/InterviewCards.jsx
@@ -2,77 +2,35 @@ import React from 'react'
 import styles from '../styles/InterviewCards.module.css'
 import Button from './Button'
 
-const InterviewCards = () => {
+const InterviewCards = ({ type, details }) => {
+    console.log(details)
     return (
         <div className={styles.cardsContainer}>
-            <div className={styles.cards}>
-                <div className={styles.cardsImage}>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Ratan_Tata_photo.jpg" alt="Candidate Profile" />
-                </div>
+            {details.map((value, id) => (
 
-                <div className={styles.cardsInfo}>
-                    <h3>Ratan Tata</h3>
-                    <p style={{ textAlign: "center" }}>Junior Scientist</p>
-                    <p>23rd June,2024@13.00 a.m</p>
-                    {/* <p>13:00 a.m</p> */}
-                    <div className={styles.cardsButton}>
-                        <Button text="Update" active="true"></Button>
-                        <Button text="Delete" active="false"></Button>
+                <div className={styles.cards} key={id}>
+                    <div className={styles.cardsImage}>
+                        <img src={value.imgLink} alt="Candidate Profile" />
+                    </div>
+
+                    <div className={styles.cardsInfo}>
+                        <h3>{value.name}</h3>
+                        <p style={{ textAlign: "center" }}>{value.jobRole}</p>
+                        {/* here display date and time for future(upcoming) interviews */}
+                        {type == "upcoming" ? <p>{value.date}@{value.time}</p> : <p>{value.time}</p>}
+                        {/* here display only time for today's(current) interviews */}
+                        <div className={styles.cardsButton}>
+                            {/* here display update and delete for future(upcoming) interviews */}
+                            {type == "upcoming" ? <Button text="Update" active="true"></Button> : <Button text="Join" active="true"></Button>}
+                            {/* here display Join and delete for today's(current)  interviews */}
+                            <Button text="Delete" active="false"></Button>
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div>
-            <div className={styles.cards}>
-                <div className={styles.cardsImage}>
-                    <img src="https://www.equitypandit.com/wp-content/uploads/2020/12/EP-Ratan-Tata.jpeg" alt="Candidate Profile" />
-                </div>
-
-                <div className={styles.cardsInfo}>
-                    <h3>Ratan Tata</h3>
-                    <p style={{ textAlign: "center" }}>Junior Scientist</p>
-                    <p>23rd June,2024@13.00 a.m</p>
-                    {/* <p>13:00 a.m</p> */}
-                    <div className={styles.cardsButton}>
-                        <Button text="Update" active="true"></Button>
-                        <Button text="Delete" active="false"></Button>
-                    </div>
-
-                </div>
-
-            </div>
-            <div className={styles.cards}>
-                <div className={styles.cardsImage}>
-                    <img src="https://www.equitypandit.com/wp-content/uploads/2020/12/EP-Ratan-Tata.jpeg" alt="Candidate Profile" />
-                </div>
-                <div className={styles.cardsInfo}>
-                    <h3>Ratan Tata</h3>
-                    <p style={{ textAlign: "center" }}>Junior Scientist</p>
-                    <p>23rd June,2024@13.00 a.m</p>
-                    {/* <p>13:00 a.m</p> */}
-                    <div className={styles.cardsButton}>
-                        <Button text="Update" active="true"></Button>
-                        <Button text="Delete" active="false"></Button>
-                    </div>
-
-                </div>
-            </div>
-            <div className={styles.cards}>
-                <div className={styles.cardsImage}>
-                    <img src="https://www.equitypandit.com/wp-content/uploads/2020/12/EP-Ratan-Tata.jpeg" alt="Candidate Profile" />
-                </div>
-                <div className={styles.cardsInfo}>
-                    <h3>Ratan Tata</h3>
-                    <p style={{ textAlign: "center" }}>Junior Scientist</p>
-                    <p>23rd June,2024@13.00 a.m</p>
-                    {/* <p>13:00 a.m</p> */}
-                    <div className={styles.cardsButton}>
-                        <Button text="Update" active="true"></Button>
-                        <Button text="Delete" active="false"></Button>
-                    </div>
-
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
